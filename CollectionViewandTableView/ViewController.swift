d07f7083f9b5cb3e31ab321976a5d4a23a72717b
藤田えりか <ericafujita@fujitaerikanomacbook-pro.local>
2019-02-15 01:08:49 +0900

    Initial Commit

---
+++ CollectionViewandTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewandTableView
+//
+//  Created by 藤田えりか on 2019/02/15.
+//  Copyright © 2019 com.erica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
